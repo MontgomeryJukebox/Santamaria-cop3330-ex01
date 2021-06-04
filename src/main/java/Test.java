4776aaa98cf26aaf35e9bad2f964c69e0ecb6cc1
David Santamaria <davidsantamaria1245@gmail.com>
2021-06-03 23:36:29 -0400

    Upload files

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,22 +4,16 @@
  */
 /*
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
- *  Copyright 2021 David Santamaria
- */
-/*
- *  UCF COP3330 Summer 2021 Assignment 1 Solution
- *  Copyright 2021 first_name last_name
- */
-/*
- *  UCF COP3330 Summer 2021 Assignment 1 Solution
  *  Copyright 2021 first_name last_name
  */
 import java.util.Scanner;
 
-public class Challenge1 {
+public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("What is your name?");
-        System.out.println("Hello, " + in.nextLine() + ", nice to meet you!");
+        String name = in.nextLine();
+        String greeting = "Hello, " + name + ", nice to meet you!";
+        System.out.println(greeting);
     }
 }
